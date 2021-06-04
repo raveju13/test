d6320e4c720e9850efefadb48adbfd2af790639d
raveju <raveju@outlook.com>
2021-06-03 20:58:12 -0500

    cambios en sitio

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Editar todo <code>src/App.js</code> and save to reload.
+          Estamos terminado el pre work con git <code>src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"
